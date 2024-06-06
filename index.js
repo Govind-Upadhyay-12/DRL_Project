d6ebd848f02c1ca31ec004b18462c2e5f77d660d
Govind <you@example.com>
2024-06-06 18:23:41 +0530

    updating_code

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
+app.use((req, res, next) => {
+    console.log('Request Origin:', req.headers.origin);
+    next();
+  });
 
 // Routes
 app.get('/', (req, res) => {
