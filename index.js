297de85376367914d2ada9b947ffc218fc4b4513
Govind <you@example.com>
2024-06-06 18:01:34 +0530

    updating_code

--- index.js
+++ index.js
@@ -2,29 +2,38 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const logger = require("morgan");
-const bodyParser = require("body-parser");
-const Db_config=require("./DB.config.js/index")
-dotenv.config();
-const app=express();
+const Db_config = require("./DB.config.js/index");
 
-Db_config()
- 
+dotenv.config();
+
+const app = express();
+
+// Connect to the database
+Db_config().then(() => {
+  console.log("Connected");
+}).catch((error) => {
+  console.error("Error connecting to the database:", error);
+  process.exit(1); 
+});
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(logger("dev"));
 
-app.get('/',(req,res)=>{
-    res.send("server is running on Aws")
-  })
-app.use(require("./routes/index.js"))
+// Routes
+app.get('/', (req, res) => {
+  res.send("Server is running on AWS");
+});
+app.use(require("./routes/index.js"));
 
-app.listen(8080, () => {
-    console.log(`Server is running on port ${8080}`);
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
-
-
-
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
