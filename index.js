ae3c62dae11142a80c888fb548168fd5fc21b133
Govind <you@example.com>
2024-06-06 19:50:14 +0530

    updating_code

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-const PORT=8080
 
-app.listen(PORT, () => {
+
+app.listen(4001, () => {
   console.log(`Server is running on port ${4004}`);
 });
